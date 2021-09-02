7899f57211f345d99d7c7042aa21765d9dcac8e3
Mohammed Adinan <adinanaries@outlook.com>
2021-09-02 00:28:39 -0400

    ...

--- src/components/bookings-container.js
+++ src/components/bookings-container.js
@@ -2,7 +2,21 @@ import BookedFlightsSearchForm from "./booked-flghts-search-form";
 import BookedHotelsSearchForm from "./booked-hotels-search-form";
 import BookedCarsSearchForm from "./booked-cars-search-form";
 
-import { show_booking_search_type_form, show_bookings_pane_main_page } from "./helper-functions";
+import { show_booking_search_type_form, show_bookings_pane_main_page, show_bookings_pane_search_results_page, show_bookings_pane_selected_results_page } from "./helper-functions";
+
+let where = "home";
+function select_booking_from_list(where_param){
+    where = where_param;
+    show_bookings_pane_selected_results_page()
+}
+
+function back_from_selected_result_pane(){
+    if(where === "home"){
+        show_bookings_pane_main_page();
+    }else if(where === "results"){
+        show_bookings_pane_search_results_page();
+    }
+}
 
 let BookingsContainer = ()=>{
     return(
@@ -34,9 +48,60 @@ let BookingsContainer = ()=>{
 
                         </div>
                         <div className="booking-pane-search-inputs-area-other-section">
-                            <p style={{fontSize: 14, color: "red", fontWeight: "bolder", letterSpacing: 1}}>
-                                <i style={{marginRight: 5}} className="fa fa-exclamation"></i>
-                                Alert</p>
+                            <div style={{fontSize: 13, fontWeight: "bolder", letterSpacing: 1, display: "flex", justifyContent: "space-between"}}>
+                                <div style={{marginTop: 5}}>
+                                    <i style={{marginRight: 5, color: "blue"}} className="fa fa-history"></i>
+                                    Last Viewed</div>
+                                <div style={{marginLeft: 20, marginTop: -5, marginBottom: 5, backgroundColor: "slateblue", padding: "10px 20px", color: "white", cursor: "pointer", fontWeight: "initial", borderRadius: 6}}>
+                                    view again</div>
+                            </div>
+                                <div style={{backgroundColor: "rgb(43,23,0)",borderRadius: 6, padding: 10}}>
+                                <p style={{color: "white", margin: 5, fontSize: 13, fontWeight: "bolder", letterSpacing: 1}}>
+                                    <i style={{color: "crimson", marginRight: 10, fontSize: 17}} className="fa fa-plane"></i>
+                                    Fight Booking
+                                </p>
+                                <div className="booking-status-tracker">
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid lightgreen"}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "green", right: "calc(100% - 14px)"}}>
+                                            <i className="fa fa-user"></i>
+                                        </div>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "green"}}>
+                                            <i className="fa fa-ticket"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title" style={{display: "flex", justifyContent: "space-between", width: "calc(100% + 40px)", marginLeft: -20}}>
+                                            <span>Booked</span> <span>Ticketed</span></p>
+                                    </div>
+                                    <div  className="booking-status-tracker-stage"style={{borderTop: "5px solid gold",}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "goldenrod"}}>
+                                            <i className="fa fa-briefcase"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Checked-in</p>
+                                    </div>
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid orange",}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "orangered"}}>
+                                        <i className="fa fa-plane"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Boarded</p>
+                                    </div>
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid red",}}>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Flown</p>
+                                        <div className="booking-status-tracker-stage-point active animated-status-icon">
+                                            <i className="fa fa-check"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style={{letterSpacing: 1, padding: 5, paddingLeft: 15, borderLeft: "3px solid orange", backgroundColor: "rgba(255,255,255,0.1)", color: "white", fontSize: 13}}>
+                                    <p style={{letterSpacing: 1, color: "white", fontSize: 13}}>
+                                        Confirmation: 
+                                        <span style={{marginLeft: 10, color: "orange"}}>
+                                            97362223673836
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -65,7 +130,7 @@ let BookingsContainer = ()=>{
                         </div>
                     </div>
                     <table className="bookings-pane-booking-list">
-                        <tr>
+                        <tr className="header">
                             <td className="header">
                                 Type
                             </td>
@@ -83,7 +148,7 @@ let BookingsContainer = ()=>{
                             </td>
                             <td className="header"></td>
                         </tr>
-                        <tr>
+                        <tr onClick={()=>select_booking_from_list("home")}>
                             <td className="bookings-pane-booking-list-column first booking-type-col">
                                 flight
                             </td>
@@ -103,7 +168,7 @@ let BookingsContainer = ()=>{
                                 <i className="fa fa-pencil" aria-hidden="true"></i>
                             </td>
                         </tr>
-                        <tr>
+                        <tr onClick={()=>select_booking_from_list("home")}>
                             <td className="bookings-pane-booking-list-column first booking-type-col">
                                 flight
                             </td>
@@ -123,7 +188,7 @@ let BookingsContainer = ()=>{
                                 <i className="fa fa-pencil" aria-hidden="true"></i>
                             </td>
                         </tr>
-                        <tr>
+                        <tr onClick={()=>select_booking_from_list("home")}>
                             <td className="bookings-pane-booking-list-column first booking-type-col">
                                 hotel
                             </td>
@@ -143,7 +208,7 @@ let BookingsContainer = ()=>{
                                 <i className="fa fa-pencil" aria-hidden="true"></i>
                             </td>
                         </tr>
-                        <tr>
+                        <tr onClick={()=>select_booking_from_list("home")}>
                             <td className="bookings-pane-booking-list-column first booking-type-col">
                                 flight
                             </td>
@@ -198,7 +263,7 @@ let BookingsContainer = ()=>{
                             </div>
                         </div>
                         <table className="bookings-pane-booking-list">
-                            <tr>
+                            <tr className="header">
                                 <td className="header">
                                     Type
                                 </td>
@@ -216,7 +281,7 @@ let BookingsContainer = ()=>{
                                 </td>
                                 <td className="header"></td>
                             </tr>
-                            <tr>
+                            <tr onClick={()=>select_booking_from_list("results")}>
                                 <td className="bookings-pane-booking-list-column first booking-type-col">
                                     One-way
                                 </td>
@@ -236,7 +301,7 @@ let BookingsContainer = ()=>{
                                     <i className="fa fa-pencil" aria-hidden="true"></i>
                                 </td>
                             </tr>
-                            <tr>
+                            <tr onClick={()=>select_booking_from_list("results")}>
                                 <td className="bookings-pane-booking-list-column first booking-type-col">
                                     Rount-trip
                                 </td>
@@ -256,7 +321,7 @@ let BookingsContainer = ()=>{
                                     <i className="fa fa-pencil" aria-hidden="true"></i>
                                 </td>
                             </tr>
-                            <tr>
+                            <tr onClick={()=>select_booking_from_list("results")}>
                                 <td className="bookings-pane-booking-list-column first booking-type-col">
                                     Multi-city
                                 </td>
@@ -276,7 +341,7 @@ let BookingsContainer = ()=>{
                                     <i className="fa fa-pencil" aria-hidden="true"></i>
                                 </td>
                             </tr>
-                            <tr>
+                            <tr onClick={()=>select_booking_from_list("results")}>
                                 <td className="bookings-pane-booking-list-column first booking-type-col">
                                     Round-trip
                                 </td>
@@ -547,6 +612,89 @@ let BookingsContainer = ()=>{
                                 </td>
                             </tr>
                         </table>
+                    </div>
+                </div>
+            </div>
+            <div id="booking-container-selected-results-pane" style={{display: "none"}}>
+                <div className="main-seaction-containers">
+                    <div style={{display: "flex"}}>
+                        <div onClick={back_from_selected_result_pane} className="standard-button" style={{marginRight: 10}}>
+                            <i style={{color: "lightgreen", marginRight: 10}} className="fa fa-arrow-left"></i>
+                            Back
+                        </div>
+                        <div onClick={show_bookings_pane_main_page} className="standard-button">
+                            <i style={{color: "lightgreen", marginRight: 10}} className="fa fa-home"></i>
+                            Home
+                        </div>
+                    </div>
+                    <div className="general-two-flex-children">
+                        <div className="general-two-flex-child first shaded-info-center">
+                            
+                            <div style={{backgroundColor: "rgb(43,23,0)",borderTopLeftRadius: 6, borderTopRightRadius: 6, borderBottom: "4px solid rgba(255,255,255,0.4)", padding: 10, marginBottom: 10}}>
+                                <p style={{color: "white", margin: 5, fontSize: 13, fontWeight: "bolder", letterSpacing: 1}}>
+                                    <i style={{color: "crimson", marginRight: 10, fontSize: 17}} className="fa fa-plane"></i>
+                                    Fight Booking
+                                </p>
+                                <div className="booking-status-tracker">
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid lightgreen"}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "green", right: "calc(100% - 14px)"}}>
+                                            <i className="fa fa-user"></i>
+                                        </div>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "green"}}>
+                                            <i className="fa fa-ticket"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title" style={{display: "flex", justifyContent: "space-between", width: "calc(100% + 40px)", marginLeft: -20}}>
+                                            <span>Booked</span> <span>Ticketed</span></p>
+                                    </div>
+                                    <div  className="booking-status-tracker-stage"style={{borderTop: "5px solid gold",}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "goldenrod"}}>
+                                            <i className="fa fa-briefcase"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Checked-in</p>
+                                    </div>
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid orange",}}>
+                                        <div className="booking-status-tracker-stage-point" style={{backgroundColor: "orangered"}}>
+                                        <i className="fa fa-plane"></i>
+                                        </div>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Boarded</p>
+                                    </div>
+                                    <div className="booking-status-tracker-stage" style={{borderTop: "5px solid red",}}>
+                                        <p className="booking-status-tracker-stage-title">
+                                            Flown</p>
+                                        <div className="booking-status-tracker-stage-point active animated-status-icon">
+                                            <i className="fa fa-check"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div style={{letterSpacing: 1, padding: 10, paddingLeft: 15, borderLeft: "3px solid orange", backgroundColor: "rgba(255,255,255,0.1)", color: "white", fontSize: 13}}>
+                                    <p style={{letterSpacing: 1, color: "white", fontSize: 13, marginBottom: 5}}>
+                                        Status: 
+                                        <span style={{marginLeft: 10, color: "orange"}}>
+                                            Flown
+                                        </span>
+                                    </p>
+                                    <p style={{letterSpacing: 1, color: "white", fontSize: 13, marginBottom: 5}}>
+                                        Confirmation: 
+                                        <span style={{marginLeft: 10, color: "orange"}}>
+                                            97362223673836
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                            
+                            <p className="page-data-info-p">
+                                Type: 
+                                <span>
+                                    <i style={{color: "crimson", marginRight: 5}} className="fa fa-plane"></i>
+                                    Fights
+                                </span>
+                            </p>
+                        </div>
+                        <div className="general-two-flex-child second shaded-info-center">
+
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/helper-functions.js
+++ src/components/helper-functions.js
@@ -87,12 +87,20 @@ export function toggle_show_main_sections(type){
 
 export function show_bookings_pane_search_results_page(){
     document.getElementById("bookings-container-main-pane").style.display = "none";
+    document.getElementById("booking-container-selected-results-pane").style.display = "none";
     $("#booking-container-search-results-pane").toggle("up");
 }
 
 export function show_bookings_pane_main_page(){
     document.getElementById("booking-container-search-results-pane").style.display = "none";
+    document.getElementById("booking-container-selected-results-pane").style.display = "none";
     $("#bookings-container-main-pane").toggle("up");
+}
+
+export function show_bookings_pane_selected_results_page(){
+    document.getElementById("bookings-container-main-pane").style.display = "none";
+    document.getElementById("booking-container-search-results-pane").style.display = "none";
+    $("#booking-container-selected-results-pane").toggle("up");
 }
 
 /*$(".box").animate({
