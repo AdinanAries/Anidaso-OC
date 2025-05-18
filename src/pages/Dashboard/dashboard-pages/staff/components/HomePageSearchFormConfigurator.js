e98d4d439c40f0746770d90d434ade1fcb17b1a0
Dev Moh <adinanaries@outlook.com>
2025-05-18 18:59:41 -0400

    ...

--- src/pages/Dashboard/dashboard-pages/staff/components/HomePageSearchFormConfigurator.js
+++ src/pages/Dashboard/dashboard-pages/staff/components/HomePageSearchFormConfigurator.js
@@ -323,9 +323,9 @@ const HomePageSearchFormConfigurator = (props) => {
                 </div>
             </div>
             <div style={{width: "calc(50% - 5px", marginBottom: 10}}>
-                <p style={{color: "white", fontSize: 14, marginTop: 10}}>
+                <p style={{color: "white", fontSize: 14, marginTop: 15, fontWeight: "bolder"}}>
                     Integrate in two easy steps!</p>
-                <p style={{fontSize: 13, backgroundColor: "white", padding: 10, marginTop: 10, boxShadow: "1px 2px 3px rgba(0,0,0,0.8)"}}>
+                <p style={{fontSize: 13, backgroundColor: "white", padding: 10, marginTop: 15, boxShadow: "1px 2px 3px rgba(0,0,0,0.8)"}}>
                     By adding the following two lines of code into your website, your booking engine search form will be added on the website.
                     Afterwards, you may edit the design of your search form with the tools on this page.
                 </p>
